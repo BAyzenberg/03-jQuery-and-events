496090a45dbbb21e89eb2b8ea59ff3d8d5e7ddd0
Benjamin <ayzenbergb@gmail.com>
2017-03-15 17:37:17 -0700

    fadein fadeout functional

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -24,12 +24,13 @@ articleView.handleAuthorFilter = function() {
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
-      console.log($(this).val())
-      $('#articles').fadeOut('fast');
-      $('allArticles').find($(this).val()).fadeIn('fast');
+      $('article').fadeOut('fast');
+      $('article[data-author="' + $(this).val() + '"]').fadeIn('fast');
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
+      $('article').fadeIn('fast');
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
