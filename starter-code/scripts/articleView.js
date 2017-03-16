afe161a7405c4daa22adbfb8ddae53c205d45e0b
Carlo Santos <carlox7@gmail.com>
2017-03-16 10:16:25 -0700

    removed old code

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -56,7 +56,7 @@ articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
     $('#' + $(this).attr('data-content')).show();
-    //$('.tab [data-content="' + $(this.target) + '"]').show();
+
     /* TODO:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
